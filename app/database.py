2240c8555cb4f590ab7e34730415e62ffb75f879
Ayrton Iseri <ayrtoniseri@hotmail.com>
2022-12-15 23:39:20 -0300

    Added environment file to prevent security leaks as well as to help production deployment.

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import env_settings
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{env_settings.database_username}:{env_settings.database_password}@{env_settings.database_hostname}:{env_settings.database_port}/{env_settings.database_name}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Dependency
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
